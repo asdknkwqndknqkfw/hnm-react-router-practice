a53329fef6f208e00b6d816ff3060f04b9470161
asdknkwqndknqkfw <codemonkey99999@gmail.com>
2025-04-08 18:56:28 +0900

    API 부르기

---
+++ src/component/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://image.hm.com/assets/hm/81/18/81185535deab1af2b84be8926c8eadbd4632bf9f.jpg?imwidth=256'></img>
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코드</div>
+      <div>99900원</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    try {
+      let response = await axios.get(url);
+      setProductList(response.data);
+    } catch (error) {
+      console.log('getProducts error', error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  
   return (
     <div>
-      <h1>상품 전체 페이지</h1>
+      <ProductCard />
     </div>
   )
 }
