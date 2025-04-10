5d1525b0c3f904e69eaa7e74630d396ebf73dc49
asdknkwqndknqkfw <codemonkey99999@gmail.com>
2025-04-11 05:46:19 +0900

    12. 상품 검색하기 2탄

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
-const Navbar = () => {
+const Navbar = ({ authenticate, setAuthenticate }) => {
   const menuList = ["여성", "Divided", "남성", "신생아/유아", "아동", "H&M Home", "Sale", "지속가능성"]
   const navigate = useNavigate();
 
@@ -17,13 +17,29 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className="login-button" onClick={() => navigate("/login")}>
-        <FontAwesomeIcon icon={faUser} />
-        <div>로그인</div>
-      </div>
+      { authenticate ? (
+          <div className="login-button" onClick={() => setAuthenticate(false)}>
+            <div>로그아웃</div>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+        ) : (
+          <div className="login-button" onClick={() => navigate("/login")}>
+            <div>로그인</div>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+        )
+      }
 
       <div className="nav-section">
-        <img width={100} height={100} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/800px-H%26M-Logo.svg.png" />
+        <Link to="/">
+          <img 
+            width={100} 
+            height={100} 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/800px-H%26M-Logo.svg.png" 
+            alt="H&M Logo"
+            style={{ cursor: 'pointer' }}
+          />
+        </Link>
       </div>
       
       <div className="menu-area">
--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -2,12 +2,17 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import ProductCard from '../component/ProductCard';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const location = useLocation();
+  const [query, setQuery] = useState('');
 
   const getProducts = async () => {
-    // let url = 'http://localhost:5000/products';
-    let url = 'https://my-json-server.typicode.com/asdknkwqndknqkfw/hnm-react-router-practice/db';
+    const query = new URLSearchParams(location.search);
+    let searchQuery = query.get('q') || '';
+    console.log('searchQuery: ', searchQuery);
+    let url = 'https://my-json-server.typicode.com/asdknkwqndknqkfw/hnm-react-router-practice/db?q=${searchQuery}';
     try {
       let response = await axios.get(url);
       console.log('response: ', response);
@@ -20,7 +25,7 @@ const ProductAll = () => {
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
   
   return (
     <div>
